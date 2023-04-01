9113dad81c9a0fdf03bd4f8905e148e0eafec285
Gabrieltr1 <gbbolonhez@gmail.com>
2023-03-31 22:05:54 -0300

    Alteração do js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ let numeros = [2,4,6,8,10]
 // Percorrendo a lista
 for(let i = 0; i < numeros.length; i++){
   if(numeros[i] % 2 === 0){
-    console.log(numero[i] + " é um núerro par")
+    console.log(numero[i] + " é um núemero impar")
   }else{
-    console.log(numero[i] + " é um número ímpar")
+    console.log(numero[i] + " é um número par")
   }
 }
+
